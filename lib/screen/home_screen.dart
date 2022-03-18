f36b70af42eb7063ecfa479948feee58e239b8a7
JC <withj1123@gmail.com>
2022-03-18 18:40:31 +0900

    6_buildilng_category_card

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dusty_dust/component/main_app_bar.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,70 @@ class HomeScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           MainAppBar(),
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 8.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                  color: lightColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: darkColor,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16.0),
+                            topRight: Radius.circular(16.0),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text(
+                            '종류별 통계',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          MainStat(
+                            category: '미세먼지',
+                            imgPath: 'asset/img/best.png',
+                            level: '최고',
+                            stat: '0㎍/㎥',
+                          ),
+                          MainStat(
+                            category: '미세먼지',
+                            imgPath: 'asset/img/best.png',
+                            level: '최고',
+                            stat: '0㎍/㎥',
+                          ),
+                          MainStat(
+                            category: '미세먼지',
+                            imgPath: 'asset/img/best.png',
+                            level: '최고',
+                            stat: '0㎍/㎥',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
