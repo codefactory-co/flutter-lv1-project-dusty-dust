d175b8dd01c95c272e2a372eced88eb0845b1bad
JC <withj1123@gmail.com>
2022-03-18 19:55:10 +0900

    8_hourly_card

--- lib/component/category_card.dart
+++ lib/component/category_card.dart
@@ -1,3 +1,5 @@
+import 'package:dusty_dust/component/card_title.dart';
+import 'package:dusty_dust/component/main_card.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
@@ -9,39 +11,14 @@ class CategoryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 160,
-      child: Card(
-        margin: EdgeInsets.symmetric(horizontal: 8.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(16.0),
-          ),
-        ),
-        color: lightColor,
+      child: MainCard(
         child: LayoutBuilder(
           builder: (context, constraint) {
-
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: darkColor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16.0),
-                      topRight: Radius.circular(16.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      '종류별 통계',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+                CardTitle(
+                  title: '종류별 통계',
                 ),
                 Expanded(
                   child: ListView(
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:dusty_dust/component/card_title.dart';
 import 'package:dusty_dust/component/category_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_card.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
@@ -21,6 +23,60 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 CategoryCard(),
+                const SizedBox(height: 16.0),
+                MainCard(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CardTitle(
+                        title: '시간별 미세먼지',
+                      ),
+                      Column(
+                        children: List.generate(
+                          24,
+                          (index) {
+                            final now = DateTime.now();
+                            final hour = now.hour;
+                            int currentHour = hour - index;
+
+                            // 3 2 1 0 23 22 21
+                            if (currentHour < 0) {
+                              currentHour += 24;
+                            }
+
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0,
+                                vertical: 4.0,
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      '$currentHour시',
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Image.asset(
+                                      'asset/img/good.png',
+                                      height: 20.0,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      '좋음',
+                                      textAlign: TextAlign.right,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
