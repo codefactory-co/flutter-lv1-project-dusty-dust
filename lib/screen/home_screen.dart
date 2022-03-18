3120f89a115d8f330f6beeb072fad2b4d3ba6e94
JC <withj1123@gmail.com>
2022-03-18 17:32:08 +0900

    5_designing_drawer

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      drawer: MainDrawer(),
+      body: CustomScrollView(
+        slivers: [
+          MainAppBar(),
+        ],
+      ),
+    );
+  }
+}
