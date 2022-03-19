7ca4b82b75024dae0eff08dae5effbd706208572
JC <withj1123@gmail.com>
2022-03-19 21:57:24 +0900

    20_making_multiple_async_requests_at_the_same_time

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -25,10 +25,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String region = regions[0];
 
-  Future<List<StatModel>> fetchData() async {
-    final statModels = await StatRepository.fetchData();
+  Future<Map<ItemCode, List<StatModel>>> fetchData() async {
+    Map<ItemCode, List<StatModel>> stats = {};
 
-    return statModels;
+    List<Future> futures = [];
+
+    for (ItemCode itemCode in ItemCode.values) {
+      futures.add(
+        StatRepository.fetchData(
+          itemCode: itemCode,
+        ),
+      );
+    }
+
+    final results = await Future.wait(futures);
+
+    for(int i = 0; i < results.length; i ++){
+      final key = ItemCode.values[i];
+      final value = results[i];
+
+      stats.addAll({
+        key: value,
+      });
+    }
+
+    return stats;
   }
 
   @override
@@ -37,14 +58,14 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: primaryColor,
       drawer: MainDrawer(
         selectedRegion: region,
-        onRegionTap: (String region){
+        onRegionTap: (String region) {
           setState(() {
             this.region = region;
           });
           Navigator.of(context).pop();
         },
       ),
-      body: FutureBuilder<List<StatModel>>(
+      body: FutureBuilder<Map<ItemCode, List<StatModel>>>(
         future: fetchData(),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
@@ -61,13 +82,13 @@ class _HomeScreenState extends State<HomeScreen> {
             );
           }
 
-          List<StatModel> stats = snapshot.data!;
-          StatModel recentStat = stats[0];
+          Map<ItemCode, List<StatModel>> stats = snapshot.data!;
+          StatModel pm10RecentStat = stats[ItemCode.PM10]![0];
 
           // 1 - 5, 6 - 10, 11 - 15
           // 7
           final status = DataUtils.getStatusFromItemCodeAndValue(
-            value: recentStat.seoul,
+            value: pm10RecentStat.seoul,
             itemCode: ItemCode.PM10,
           );
 
@@ -75,7 +96,7 @@ class _HomeScreenState extends State<HomeScreen> {
             slivers: [
               MainAppBar(
                 region: region,
-                stat: recentStat,
+                stat: pm10RecentStat,
                 status: status,
               ),
               SliverToBoxAdapter(
